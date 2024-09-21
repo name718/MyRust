cd3f2e6df0e42070da590aa949198990de59f15b
majuntao <1547996098@qq.com>
2024-09-21 23:52:18 +0800

    找出出现至少三次的最长特殊子字符串 I

--- leetCode/src/main.rs
+++ leetCode/src/main.rs
@@ -1,3 +1,44 @@
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn maximum_length(s: String) -> i32 {
+        let n = s.len();
+        let mut max_length = -1;
+        let mut count_map = HashMap::new();
+
+        let mut i = 0;
+        while i < n {
+            let char = s.chars().nth(i).unwrap();
+            let mut length = 0;
+
+            while i < n && s.chars().nth(i).unwrap() == char {
+                length += 1;
+                i += 1;
+            }
+
+            if length > 1 {
+                *count_map.entry(length).or_insert(0) += 1;
+            }
+        }
+
+        for (&length, &count) in count_map.iter() {
+            if count >= 3 {
+                max_length = max_length.max(length);
+            }
+        }
+
+        max_length
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    test1();
+}
+
+fn test1() {
+    let s = String::from("aaaaaa");
+    let result = Solution::maximum_length(s);
+    println!("{}", result);
 }
