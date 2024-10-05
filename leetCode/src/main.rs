439b7abbe3745bf58252c17e26ca4023a4245c52
majuntao <1547996098@qq.com>
2024-10-05 12:34:58 +0800

    380. O(1) 时间插入、删除和获取随机元素

--- leetCode/src/main.rs
+++ leetCode/src/main.rs
@@ -1,10 +1,65 @@
 #![allow(unused)]
+extern crate rand;
 
 use core::num;
+use rand::Rng;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 struct Solution;
+struct RandomizedSet {
+    set:HashSet<i32>,
+    vec:Vec<i32>
+}
 
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl RandomizedSet {
+
+    fn new() -> Self {
+        RandomizedSet{
+            set:HashSet::new(),
+            vec:Vec::new(),
+        }
+    }
+    
+    fn insert(&mut self, val: i32) -> bool {
+        if self.set.contains(&val) {
+            return false;
+        }
+        self.set.insert(val);
+        self.vec.push(val);
+        true
+    }
+    
+    fn remove(&mut self, val: i32) -> bool {
+        if self.set.contains(&val) {
+            self.set.remove(&val);
+            if let Some(index) = self.vec.iter().position(|&v| v == val) {
+                self.vec.swap_remove(index);
+            }
+            return true;
+        }
+        false
+    }
+    
+    fn get_random(&self) -> i32 {
+        let mut rng = rand::thread_rng();
+        let random_index = rng.gen_range(0..self.vec.len());
+        self.vec[random_index]
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * let obj = RandomizedSet::new();
+ * let ret_1: bool = obj.insert(val);
+ * let ret_2: bool = obj.remove(val);
+ * let ret_3: i32 = obj.get_random();
+ */
 impl Solution {
     pub fn h_index(citations: Vec<i32>) -> i32 {
         // 对引用次数数组进行降序排序
@@ -220,7 +275,15 @@ fn main() {
     // test9();
     // test10();
     // test11();
-    test12();
+    // test12();
+    test13();
+}
+
+fn test13(){
+    let mut obj = RandomizedSet::new();
+    let ret_1: bool = obj.insert(1);
+    let ret_2: bool = obj.remove(1);
+    let ret_3: i32 = obj.get_random();
 }
 
 fn test12(){
